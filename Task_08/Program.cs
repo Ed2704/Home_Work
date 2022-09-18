f9c733b8492da8e7eb4465375344c04f3c566d04
Eduard <egorov-ed@mail.ru>
2022-09-18 13:14:22 +0300

    Task 08

---
+++ Task_08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число больше 1: ");
+int number = int.Parse(Console.ReadLine());
+int count =1;
+
+while (count <= number)
+{
+    if ((count % 2) == 0)
+    {
+        Console.Write (count);
+        Console.Write (", ");
+        count++;
+    }
+    count++;
+}
