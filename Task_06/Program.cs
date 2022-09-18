e6d24547fec33411683338f1a47d38e060fa60ee
Eduard <egorov-ed@mail.ru>
2022-09-18 13:09:23 +0300

    Task 06

---
+++ Task_06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((Math.Abs(number) % 2) > 0 )
+{
+    Console.WriteLine ("Нет");
+}
+else
+{
+    Console.WriteLine ("Да");
+}
