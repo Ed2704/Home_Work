67847e76f95ec4751bf44238b40fe32ae12cdd00
Eduard <egorov-ed@mail.ru>
2022-09-18 13:01:31 +0300

    Task 02

---
+++ Task_02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите первое число: ");
+int numberA = int.Parse (Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int numberB = int.Parse (Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write ("Max = ");
+    Console.Write (numberA);
+    Console.Write (" Min = ");
+    Console.Write (numberB);
+
+}
+else
+{
+    Console.Write ("Max = ");
+    Console.Write (numberB);
+    Console.Write (" Min = ");
+    Console.Write (numberA);
+}
