aec370bee8ffc7eacc8179125f7ebc7b0b6b96a4
Eduard <egorov-ed@mail.ru>
2022-09-21 13:42:00 +0300

    Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите трехзначное число: "!);
+int number = int.Parse(Console.ReadLine());
+
+int secondNumber = (number%100 -number%10)/10;
+
+Console.WriteLine("Второе число:  " +secondNumber);
