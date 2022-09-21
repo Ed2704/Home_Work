53754dcca0da6960a37b2830168189ff5b6620c7
Eduard <egorov-ed@mail.ru>
2022-09-21 23:03:14 +0300

    Task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число, не менее 3-х знаков: ");
+int number = int.Parse(Console.ReadLine());
+int signsNumbers = number;
+int i = 0;
+
+if (number < 100)
+{
+    Console.WriteLine ("Третьей цифры нет !!!");
+}
+else
+{
+while(signsNumbers > 0) 
+    {
+        i++;
+        signsNumbers/= 10;
+    }
+//Console.WriteLine("Количество знаков введенного числа : {0}", i);
+
+int pov1= (int)Math.Pow(10,(i-2));
+int pov2= (int)Math.Pow(10,(i-3));
+
+int secondNumber = (number % pov1 - number % pov2) / pov2;
+
+Console.WriteLine(secondNumber);
+}
