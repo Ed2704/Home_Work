9fda0fd14d58345bf42180ecfa4b3450f06364d2
Eduard <egorov-ed@mail.ru>
2022-09-22 11:39:56 +0300

    Task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите число от 1-7");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0 && number < 8)
+{
+    if (number == 6 || number ==7)
+    {
+        Console.WriteLine ("Ура!!! Это выходной!");
+    }
+    else
+    {
+        Console.WriteLine ("Увы ... Это будни...");
+    }
+}
+else
+{
+    Console.WriteLine ("Вы неправильно ввели число");
+}
