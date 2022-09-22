24d831a010897897689aace0f4f13098806f273c
Eduard <egorov-ed@mail.ru>
2022-09-22 20:53:00 +0300

    task 18

---
+++ Task_18/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите номер координатной четверти: ");
+int X = int.Parse(Console.ReadLine()!);
+
+if (X ==1)
+{
+    Console.WriteLine("Х > 0 и Y > 0");
+}
+
+if (X ==2)
+{
+    Console.WriteLine("Х < 0 и Y > 0");
+}
+
+if (X ==3)
+{
+    Console.WriteLine("Х < 0 и Y < 0");
+} 
+
+if (X ==4)
+{
+    Console.WriteLine("Х > 0 и Y < 0");
+}
