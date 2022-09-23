0d3504371291a39e3808b095269bbc370d7e19bd
Eduard <egorov-ed@mail.ru>
2022-09-23 09:46:45 +0300

    task 23

---
+++ Task_23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите число болше 1: ");
+int x = int.Parse (Console.ReadLine()!);
+int i = 1;
+
+while (i <= x)
+{
+  int result = (int) Math.Pow ( i, 3);
+    if (i != x)
+    {
+        Console.Write (result + ", ");
+    }
+    else
+    {  
+        Console.Write (result);
+    }
+  i++;
+}
