4de30c536d23fa1c602959f28680dd21e477f82a
Eduard <egorov-ed@mail.ru>
2022-09-23 13:06:23 +0300

    task 19

---
+++ Task_19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int signsNumbers = number;
+int i = 0;
+
+if (number < 10000 || number > 100000)
+{
+    Console.WriteLine ("Вы ввели число неправильно");
+}
+else
+{
+    int num1 = number / 10000;
+    int num2 = number % 10000 /1000;
+    int num4 = number % 100 / 10;
+    int num5 = number % 10;
+
+    if(num1 ==num5 && num2 == num4) Console.WriteLine ("Да, это палиндром");
+    else Console.WriteLine ("Нет, это не палиндром");
+}
