a6993fd8da59216c5fcd8dd509174f22b726fcee
Eduard <egorov-ed@mail.ru>
2022-09-23 09:34:16 +0300

    task 21

---
+++ Task_21/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Введите координату X первой точки ");
+double x0 = double.Parse (Console.ReadLine()!);
+Console.Write("Введите координату Y первой точки ");
+double y0 = double.Parse (Console.ReadLine()!);
+Console.Write("Введите координату Z первой точки ");
+double z0 = double.Parse (Console.ReadLine()!);
+
+Console.Write("Введите координату X второй точки ");
+double x1 = double.Parse (Console.ReadLine()!);
+Console.Write("Введите координату Y второй точки ");
+double y1 = double.Parse (Console.ReadLine()!);
+Console.Write("Введите координату Z второй точки ");
+double z1 = double.Parse (Console.ReadLine()!);
+
+double result = Math.Sqrt (Math.Pow (x1-x0,2) + 
+                Math.Pow (y1-y0,2) + 
+                Math.Pow (z1-z0,2));
+
+Console.WriteLine ("{0:0.00}", result);
