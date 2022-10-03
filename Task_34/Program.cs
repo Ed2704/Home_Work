3b0fd3bee150a1ce38a3baeef9d3970eb9b32f14
Eduard <egorov-ed@mail.ru>
2022-10-03 17:28:55 +0300

    Task 34

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -5,3 +5,36 @@
 [345, 897, 568, 234] -> 2
 */
 
+Console.Clear();
+
+Console.WriteLine("Введите массив из трехзначных чисел через пробел");
+string input = Console.ReadLine()!;
+int[] array = ParseToArray(input);
+
+
+int res = НonestNumbers(array);
+Console.WriteLine("Количество четных чисел в массиве - " + res);
+
+
+int НonestNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] ParseToArray(string str)
+{
+    string[] stringArray = str.Split(" ");
+    int[] result = new int[stringArray.Length];
+ 
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = int.Parse(stringArray[i]);
+    }
+ 
+    return result;
+}
