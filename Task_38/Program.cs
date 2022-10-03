aa792d974ca89fd6447de8a4d30f7d1165487192
Eduard <egorov-ed@mail.ru>
2022-10-03 19:09:34 +0300

    Task 38

---
+++ Task_38/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу
+между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите массив чисел через пробел");
+string input = Console.ReadLine()!;
+double[] array = ParseToArray(input);
+
+
+double res = DiffMaxMin(array);
+Console.WriteLine("Разница между максимальным и минимальным элементов массива  " + res);
+
+
+double DiffMaxMin(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    double diff= 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i] > max) max = arr[i];
+        else if (arr[i] < min) min = arr[i];
+    }
+    diff = max - min;
+    return diff;
+}
+
+double[] ParseToArray(string str)
+{
+    string[] stringArray = str.Split(" ");
+    double[] result = new double[stringArray.Length];
+ 
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        result[i] = double.Parse(stringArray[i]);
+    }
+ 
+    return result;
+}
